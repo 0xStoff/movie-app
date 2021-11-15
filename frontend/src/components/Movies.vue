c9ddb8df285911f2f3bb96fcb1d1727002b300f5
Stoeff25 <christoph.tiger@hotmail.com>
2021-11-15 14:40:09 +0100

    change from then to async

--- frontend/src/components/Movies.vue
+++ frontend/src/components/Movies.vue
@@ -1,32 +1,34 @@
 <template>
   <div>
     <h1 class="m-5">Movie App</h1>
-    <form>
-      <div style="margin: 0 auto" class="form-group w-50">
+    <div style="margin: 0 auto" class="form-group w-50">
+      <form>
         <input
           class="form-control mb-3"
           v-model="input.titleInput"
           type="text"
           placeholder="Title"
+          required
         />
         <input
           class="form-control mb-3"
           v-model="input.directorInput"
           type="text"
           placeholder="Director"
+          required
         />
         <textarea
           class="form-control mb-3"
           v-model="input.descriptionInput"
           rows="3"
           placeholder="Description"
+          required
         ></textarea>
-
         <button
+          type="submit"
           class="btn btn-primary m-3"
           v-if="!isEditing"
           @click.prevent="addFilm"
-          type="button"
         >
           Add Movie
         </button>
@@ -46,8 +48,11 @@
         >
           Update
         </button>
-      </div>
-    </form>
+      </form>
+      <button class="btn btn-danger m-1" @click="fetchData('new', 'GET')">
+        Fetch
+      </button>
+    </div>
 
     <ul>
       <li
@@ -65,11 +70,15 @@
           >
             {{ movie.description }}
           </p>
-          <button class="btn btn-danger m-1" @click="deleteMovie(index)">
-            X
-          </button>
+
           <button class="btn btn-primary m-1" @click="editMovie(movie)">
             edit
+          </button>
+          <button class="btn btn-danger m-1" @click="deleteConfirmation(index)">
+            X
+          </button>
+          <button class="btn btn-danger m-1" @click="fetchData(index, 'GET')">
+            Fetch
           </button>
         </div>
       </li>
@@ -161,9 +170,31 @@ export default {
           console.log("Auth error " + error);
         });
     },
-    addFilm() {
-      const axios = require("axios");
 
+    async fetchData(index, method, data) {
+      const axios = require("axios");
+      let id = "";
+      if (!isNaN(index)) {
+        id = this.movies[index].id;
+      }
+
+      console.log(id);
+      const response = await axios({
+        url: "http://localhost:1338/movies/" + id,
+        method: method,
+        headers: {
+          Authorization:
+            "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNjM1MTcwOTY3LCJleHAiOjE2Mzc3NjI5Njd9.fk8vfoJ7kkCLzfX0-RRGWJZBdeKq6Ip2L0DoodoC31w",
+        },
+        data: data,
+      });
+      return response;
+    },
+
+    async addFilm() {
+      const axios = require("axios");
+      let missingInput = new Boolean(false);
+      console.log(test);
       // console.log(this.movies);
       // How to add a key properly? Vue component is not rerendering when adding film, this is why window.location was used.
       // this.addedMovie = this.movies;
@@ -182,94 +213,186 @@ export default {
         director: this.input.directorInput,
         description: this.input.descriptionInput,
       };
-      // const loginInfo = {
-      //   username: "stoff",
-      //   password: "Abcd1234",
-      // };
-      // globale variable? data? andere möglichkeit?
+      let test = await this.fetchData(4, "POST");
+
+      for (const property in movie) {
+        if (`${movie[property]}` == "") {
+          this.$swal(`${property}`, "is missing", "error");
+          // alert(`${property}` + " is missing.");
+          missingInput = true;
+        }
+      }
+
+      if (missingInput == false) {
+        // const loginInfo = {
+        //   username: "stoff",
+        //   password: "Abcd1234",
+        // };
+        // globale variable? data? andere möglichkeit?
+
+        // var session_url = "http://localhost:1338/movies";
+        // var username = "stoff";
+        // var password = "Abcd1234";
+        // var credentials = btoa(username + ":" + password);
+        // var basicAuth = "Basic " + credentials;
+        // await axios
+        //   .post(
+        //     session_url,
+        //     {},
+        //     {
+        //       auth: {
+        //         username: username,
+        //         password: password,
+        //       },
+        //     }
+        //   )
+        //   .then(function (response) {
+        //     console.log("Authenticated ->" + response);
+        //   })
+        //   .catch(function (error) {
+        //     console.log("Error on Authentication ->" + error);
+        //   });
+
+        // axios
+        //   .post(
+        //     "http://localhost:1338/movies",
+        //     {
+        //       title: "POST Test title",
+        //       director: "POST Test director",
+        //       description: "POST Test description",
+        //     },
+        //     {
+        //       body: {
+        //         auth: {
+        //           identifier: "admin",
+        //           password: "Abcd1234",
+        //         },
+        //       },
+        //     }
+        //   )
+        //   .then((response) => {
+        //     console.log("SUCCCESSSSSS " + response);
+        //   })
+        //   .catch(() => {
+        //     console.log("ERROR GAGDASGASG");
+        //   });
+
+        // fetch ok? bessere möglichkeit?
+        // object destructuring? besserer weg?
+
+        const config = {
+          method: "post",
+          url: "http://localhost:1338/movies",
+          headers: {
+            Authorization:
+              "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNjM1MTcwOTY3LCJleHAiOjE2Mzc3NjI5Njd9.fk8vfoJ7kkCLzfX0-RRGWJZBdeKq6Ip2L0DoodoC31w",
+            "Content-Type": "application/json",
+          },
+          data: movie,
+        };
+        axios(config)
+          .then((response) => {
+            this.movies.push(movie);
+            this.addedMovie = null;
+            this.input.titleInput = "";
+            this.input.directorInput = "";
+            this.input.descriptionInput = "";
+            this.$swal
+              .fire({
+                icon: "success",
+                title: response.data.title + " has been created!",
+                timer: 2000,
+                timerProgressBar: true,
+                didOpen: () => {
+                  this.$swal.showLoading();
+                },
+              })
+              .then((result) => {
+                if (result.isConfirmed) {
+                  console.log("success");
+                  this.id += 1;
+                }
+              });
+            // .then(
+            //   () => window.location.reload()
+            // );
+            // Reload von window sehr unschön
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+      }
+    },
+
+    deleteConfirmation(index) {
+      const swalWithBootstrapButtons = this.$swal.mixin({
+        customClass: {
+          confirmButton: "btn btn-success m-2",
+          cancelButton: "btn btn-danger m-2",
+        },
+        buttonsStyling: false,
+      });
+      const axios = require("axios");
+      let id = this.movies[index].id;
+      let title;
       const config = {
-        method: "post",
-        url: "http://localhost:1338/movies",
+        method: "delete",
+        url: "http://localhost:1338/movies/" + id,
         headers: {
           Authorization:
             "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNjM1MTcwOTY3LCJleHAiOjE2Mzc3NjI5Njd9.fk8vfoJ7kkCLzfX0-RRGWJZBdeKq6Ip2L0DoodoC31w",
-          "Content-Type": "application/json",
         },
-        data: movie,
       };
-
-      // var session_url = "http://localhost:1338/movies";
-      // var username = "stoff";
-      // var password = "Abcd1234";
-      // var credentials = btoa(username + ":" + password);
-      // var basicAuth = "Basic " + credentials;
-      // await axios
-      //   .post(
-      //     session_url,
-      //     {},
-      //     {
-      //       auth: {
-      //         username: username,
-      //         password: password,
-      //       },
-      //     }
-      //   )
-      //   .then(function (response) {
-      //     console.log("Authenticated ->" + response);
-      //   })
-      //   .catch(function (error) {
-      //     console.log("Error on Authentication ->" + error);
-      //   });
-
-      // axios
-      //   .post(
-      //     "http://localhost:1338/movies",
-      //     {
-      //       title: "POST Test title",
-      //       director: "POST Test director",
-      //       description: "POST Test description",
-      //     },
-      //     {
-      //       body: {
-      //         auth: {
-      //           identifier: "admin",
-      //           password: "Abcd1234",
-      //         },
-      //       },
-      //     }
-      //   )
-      //   .then((response) => {
-      //     console.log("SUCCCESSSSSS " + response);
-      //   })
-      //   .catch(() => {
-      //     console.log("ERROR GAGDASGASG");
-      //   });
-
-      // fetch ok? bessere möglichkeit?
-      // object destructuring? besserer weg?
-      axios(config)
+      axios
+        .get("http://localhost:1338/movies/" + id)
         .then((response) => {
-          this.movies.push(movie);
-          this.addedMovie = null;
-          this.input.titleInput = "";
-          this.input.directorInput = "";
-          this.input.descriptionInput = "";
-          this.$swal(response.data.title, "Added successfully", "success").then(
-            () => (this.id += 1)
-          );
-          // .then(
-          //   () => window.location.reload()
-          // );
-          // Reload von window sehr unschön
+          title = response.data.title;
         })
-        .catch((error) => {
-          console.log(error);
+        .then(() => {
+          swalWithBootstrapButtons
+            .fire({
+              title: `Are you sure? ${title}`,
+              text: "You won't be able to revert this!",
+              icon: "warning",
+              showCancelButton: true,
+              confirmButtonText: "Yes, delete it!",
+              cancelButtonText: "No, cancel!",
+              reverseButtons: true,
+            })
+            .then((result) => {
+              if (result.isConfirmed) {
+                axios(config)
+                  .then((response) => {
+                    swalWithBootstrapButtons.fire({
+                      icon: "error",
+                      title: response.data.title + " has been deleted!",
+                      timer: 2000,
+                      timerProgressBar: true,
+                      didOpen: () => {
+                        this.$swal.showLoading();
+                      },
+                    });
+                  })
+                  .catch((error) => {
+                    console.log(error);
+                  });
+
+                this.movies.splice(index, 1);
+              } else if (
+                /* Read more about handling dismissals below */
+                result.dismiss === this.$swal.DismissReason.cancel
+              ) {
+                swalWithBootstrapButtons.fire(
+                  "Cancelled",
+                  "Your Movie is safe :)",
+                  "error"
+                );
+              }
+            });
         });
     },
 
     deleteMovie(index) {
-      console.log(index);
-
       const axios = require("axios");
       let id = this.movies[index].id;
       const config = {
